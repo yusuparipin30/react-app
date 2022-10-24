228ba8b32d2e2550cd9d1a6a2d474353d32cd7fd
yusuparipin30 <yusuparipin30@gmail.com>
2022-10-24 13:45:40 +0700

    done react-js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //untuk menggunakan useState harus mengimport dari react library
 //import bberapa fungsi dari react router-dom
 //import Header from "./components/Header"
-//import { useState } from "react"
+import { useState } from "react"
 //import {useEffect } from "react"
-//import ProductList from "./components/ProductList";
-//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-//import About from "./components/About";
-//import Contact from "./components/Contact";
+import ProductList from "./components/ProductList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 
 
@@ -174,46 +174,47 @@
     //CEK PADA PACKAGE-LOCK.JSON sudah terinstal apa blm
     //pertama2 import dan instal beberapa fungsi 
 
-    // function App() {
-    //   const [products, setProducts] = useState([
-    //     {id: 1, title: 'Product 1', price: 899},
-    //     {id: 2, title: 'Product 2', price: 982},
-    //     {id: 3, title: 'Product 3', price: 322},
-    //     {id: 4, title: 'Product 4', price: 763},
-    //     {id: 5, title: 'Product 5', price: 389}
-    //   ]);
+    function App() {
+      const [products, setProducts] = useState([
+        {id: 1, title: 'Product 1', price: 899},
+        {id: 2, title: 'Product 2', price: 982},
+        {id: 3, title: 'Product 3', price: 322},
+        {id: 4, title: 'Product 4', price: 763},
+        {id: 5, title: 'Product 5', price: 389}
+      ]);
      
-    //   const deleteProduct = (productId) => {
-    //     const newProducts = products.filter(product => product.id !== productId);
-    //     setProducts(newProducts);
-    //   }
+      const deleteProduct = (productId) => {
+        const newProducts = products.filter(product => product.id !== productId);
+        setProducts(newProducts);
+      }
 
-    //       //di dalam router gunakan fungsi switch
-    //       //didalam switch gunakan route yang di instal dari react router dom
-    //       //di dalam route terdapat atribut path yaitu url
-    //       //panggil komponen pada saat mengunjungi slash
-    //       //non aktifkan import header karna tidak di butuhkan
-    //       return (
-    //         <div>
-    //           <Router>
-    //             <Switch>
-    //               <Route exact path="/">
-    //                 <ProductList products={ products } deleteProduct={ deleteProduct } />
-    //               </Route>
-    //               <Route path="/about">
-    //                 <About />
-    //               </Route>
-    //               <Route path="/contact">
-    //                 <Contact />
-    //               </Route>
-    //             </Switch>
-    //           </Router>
-    //         </div>
-    //       );
-    //     }
+          //di dalam router gunakan fungsi switch
+          //didalam switch gunakan route yang di instal dari react router dom
+          //di dalam route terdapat atribut path yaitu url
+          //panggil komponen pada saat mengunjungi slash
+          //non aktifkan import header karna tidak di butuhkan
+          return (
+            <div>
+              <Router>
+
+                <Routes>
+
+                <Route exact path="/" element={<ProductList/>} products={ products } deleteProduct={ deleteProduct }/>
+                  
+                <Route path="/about"  element={<About/>}/> 
+                
+                <Route path="/contact" element={<Contact/>}/> 
+              
+                </Routes>
+
+              </Router>
+            </div>
+          );
+        }
 
 
     //7.JSON SERVER
+    //membuai API palsu
     //buat file db.json dan isi filenya
     /*buka terminal baru dan jalankan perintah berikut:
 
@@ -222,6 +223,8 @@ Kembali ke browser dan kunjungi URL berikut:
 
 
 http://localhost:8080/products */
+
+ 
 
 
 export default App;
